a77a378ea71c20158e4d55dcb4f8aba713365162
Ishank Gupta <ishank.ny@gmail.com>
2021-05-19 17:24:28 +0530

    Added scaffold for blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :blogs
+  # get "blogs/new", to: "blogs#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get "/users"
 end
