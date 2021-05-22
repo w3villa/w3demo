e4fb3d5ecb0fe87ed9206791e64d8009b700df67
Ishank Gupta <ishank.ny@gmail.com>
2021-05-22 18:12:01 +0530

    Added devise

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,10 @@
 class Blog < ApplicationRecord
+
+	# validates :title, presence: true
+  validates :title, length: { minimum: 10 }
+	validates :body, presence: true
+	validates :user_id, numericality: true
+
+	belongs_to :user
+
 end
