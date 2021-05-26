9cad303c7be527d80f85df7a0a12bd9c82ce46ca
mariyam <mariyam.kauser@w3villa.com>
2021-05-26 16:19:46 +0530

    category addon and layout design

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,7 @@
 class Blog < ApplicationRecord
 
 	# validates :title, presence: true
-  validates :title, length: { minimum: 10 }
+  validates :title, length: { minimum: 1 }
 	validates :body, presence: true
 	validates :user_id, numericality: true
 
