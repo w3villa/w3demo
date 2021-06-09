2d05b3ce23a0c73b5f32424c67d18d4cd2ba8711
mariyam <mariyam.kauser@w3villa.com>
2021-06-09 15:38:26 +0530

    dashboard layout

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,6 +1,24 @@
 class Admin::DashboardsController < Admin::ApplicationController
 
  def index
+     @admin = current_user.name
+      @blogs = Blog.all
+      @categories = Category.all  
+      @tags = Tag.all  
+      @users = User.all
+     
+  end
+  def blogs
+    @admin = current_user.name
+    @blogs = Blog.all
    
+  end
+  def category
+    @admin = current_user.name
+    @categories = Category.all  
+  end
+   def tag
+    @admin = current_user.name
+    @tags = Tag.all  
   end
 end
