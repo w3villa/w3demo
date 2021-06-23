a042fdeebd90eba29ab3e1894da5649926ccb521
w3villa-Diwakar <diwaker.singh@w3villa.com>
2021-06-23 18:10:49 +0530

    ready

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,20 @@
 
   before_action :find_commentable
 
+
+     def index
+      @comments = Comment.all
+     end
     def new
       @comment = Comment.new
     end
+    def show
+      @comment = Comment.find(params[:id])
+    end
+    def edit
+      @comment = Comment.find(params[:id])
+    end
+
 
     def create
 
@@ -16,6 +27,28 @@
       end
     end
 
+    def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment, notice: "Comment was successfully updated." }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /comments/1 or /comments/1.json
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to comments_url, notice: "Comment was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
     private
 
     def comment_params
@@ -23,7 +56,8 @@
     end
 
     def find_commentable
+
       @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
-      @commentable = Blog.find_by_id(params[:blog_id]) if params[:blog_id]
+      @commentable = Blog.friendly.find_by_id(params[:blog_id]) if params[:blog_id]
     end
   end
