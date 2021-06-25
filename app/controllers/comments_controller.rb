7b73ec9a0a06f12a24116d0ca3ab4e533c405b07
w3villa-Diwakar <diwaker.singh@w3villa.com>
2021-06-25 14:54:41 +0530

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,38 @@
 class CommentsController < ApplicationController
 
   before_action :find_commentable
+  before_action :set_comment, only: %i[ show edit update destroy ]
+
+  def index
+    @comments = Comment.all
+  end
+
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment, notice: "Comment was successfully updated." }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to comments_url, notice: "Comment was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
 
     def new
       @comment = Comment.new
@@ -26,4 +58,10 @@ class CommentsController < ApplicationController
       @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
       @commentable = Blog.friendly.find(params[:blog_id]) if params[:blog_id]
     end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+
   end
